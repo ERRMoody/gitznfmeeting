ef3982f4b4686a5248d14dfb14e036eecc66d317
Edmund Moody <em16077@bc4login5.bc4.acrc.priv>
2019-08-23 13:18:06 +0100

    new file for tree

---
+++ finalselection/lgc60/iqtree.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --job-name=zfml
+#SBATCH --partition=hmem
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=28
+#SBATCH --time=14-00:00:0
+
+module load apps/iqtree/omp-1.6.7
+
+iqtree -s representativeselectionznf648.fa -m LG+C60+F+G -bb 1000 -wbtl -bnni -nt AUTO --runs 10
+
+
